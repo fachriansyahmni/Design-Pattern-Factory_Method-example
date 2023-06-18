92ca2356db2cf9bbbb54e0c917d4f3c9cb1de510
fachriansyahmni <fachriansyahmni@gmail.com>
2023-06-19 04:52:17 +0700

    done

---
+++ src/Kubus.java
@@ -0,0 +1,35 @@
+public class Kubus implements BangunRuang {
+    private double panjang;
+    private double lebar;
+    private double tinggi;
+
+    public double getPanjang() {
+        return panjang;
+    }
+
+    public void setPanjang(double panjang) {
+        this.panjang = panjang;
+    }
+
+    public double getLebar() {
+        return lebar;
+    }
+
+    public void setLebar(double lebar) {
+        this.lebar = lebar;
+    }
+
+    public double getTinggi() {
+        return tinggi;
+    }
+
+    public void setTinggi(double tinggi) {
+        this.tinggi = tinggi;
+    }
+
+    @Override
+    public void gambarBangun() {
+        System.out.println("Menggambar kubus");
+    }
+
+}
